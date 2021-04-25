7ef40e6a4fa94e16639b23846656a4b660983eb0
Yuri Goncharuk <yukon39@gmail.com>
2021-04-25 13:00:06 +0300

    fix: remove OneScript reference

--- src/DebugBSL/debugger/debugBaseData/AttachDebugUIResult.cs
+++ src/DebugBSL/debugger/debugBaseData/AttachDebugUIResult.cs
@@ -1,30 +1,23 @@
-﻿using ScriptEngine;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace com.github.yukon39.DebugBSL.debugger.debugBaseData
 {
     [XmlType(Namespace = "http://v8.1c.ru/8.3/debugger/debugBaseData")]
-    [EnumerationType("AttachDebugUIResult", "РезультатПодключенияОтладки")]
     public enum AttachDebugUIResult
     {
         [XmlEnum(Name = "unknown")]
-        [EnumItem("Unknown", "Неизвестно")]
         Unknown,
 
         [XmlEnum(Name = "registered")]
-        [EnumItem("Registered", "Зарегистровано")]
         Registered,
 
         [XmlEnum(Name = "credentialsRequired")]
-        [EnumItem("CredentialsRequired", "ТребуетсяАвторизация")]
         CredentialsRequired,
 
         [XmlEnum(Name = "ibInDebug")]
-        [EnumItem("IBInDebug", "БазаВРежимеОтладки")]
         IBInDebug,
 
         [XmlEnum(Name = "notRegistered")]
-        [EnumItem("NotRegistered", "Незарегистровано")]
         NotRegistered
     }
 }
