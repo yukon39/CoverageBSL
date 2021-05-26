44b2577de2463a0125481255b7e3c1bba92dd85d
Yuri Goncharuk <yukon39@gmail.com>
2021-05-26 18:45:26 +0300

    feat: Add public client factory builder

---
+++ src/DebugClientBSL/DebuggerClientFactory.cs
@@ -0,0 +1,16 @@
+﻿using com.github.yukon39.DebugBSL.Client.Impl;
+using com.github.yukon39.DebugBSL.Client.Internal;
+using System;
+
+namespace com.github.yukon39.DebugBSL.Client
+{
+    public class DebuggerClientFactory
+    {
+        public static IDebuggerClient NewInstance(string debuggerURI)
+        {
+            var rootURI = new Uri(debuggerURI);
+            var executor = DebuggerClientExecutor.Create(rootURI);
+            return DebuggerClient.NewInstance(executor);
+        }
+    }
+}
