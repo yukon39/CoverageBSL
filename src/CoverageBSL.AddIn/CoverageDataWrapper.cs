f9f8116164cbb96026e0eca75796998a1c9e50ec
Yuri Goncharuk <yukon39@gmail.com>
2021-05-27 01:08:18 +0300

    refactor: Create coverage data wrapper

---
+++ src/CoverageBSL.AddIn/CoverageDataWrapper.cs
@@ -0,0 +1,34 @@
+﻿using com.github.yukon39.CoverageBSL.Coverage;
+using ScriptEngine.HostedScript.Library;
+using ScriptEngine.Machine.Contexts;
+
+namespace com.github.yukon39.CoverageBSL.AddIn
+{
+    [ContextClass(typeName: "CoverageData", typeAlias: "ДанныеПокрытия")]
+    public class CoverageDataWrapper : AutoContext<CoverageDataWrapper>, IObjectWrapper
+    {
+        private readonly CoverageData coverageData;
+
+        public CoverageDataWrapper(CoverageData coverageData) =>
+            this.coverageData = coverageData;
+
+        [ContextProperty("TotalDurability")]
+        public long TotalDurability
+        {
+            get => coverageData.TotalDurability;
+            set => coverageData.TotalDurability = value;
+        }
+
+        [ContextProperty("Data")]
+        public MapImpl Data
+        {
+            get => coverageData.Data;
+        }
+
+        [ContextMethod("Write")]
+        public void Write(string filePath) =>
+            coverageData.Write(filePath);
+
+        public object UnderlyingObject => coverageData;
+    }
+}
--- src/CoverageBSL/Coverage/CoverageData.cs
+++ src/CoverageBSL/Coverage/CoverageData.cs
@@ -1,17 +1,65 @@
-﻿using ScriptEngine.HostedScript.Library;
-using ScriptEngine.Machine.Contexts;
+﻿using com.github.yukon39.DebugBSL.debugger.debugMeasure;
+using ScriptEngine.HostedScript.Library;
+using ScriptEngine.HostedScript.Library.Json;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Values;
+using System.Collections.Generic;
 
 namespace com.github.yukon39.CoverageBSL.Coverage
 {
-    [ContextClass(typeName: "CoverageData", typeAlias: "ДанныеПокрытия")]
-    public class CoverageData : AutoContext<CoverageData>
+    public class CoverageData
     {
-        [ContextProperty("TotalDurability")]
         public long TotalDurability { get; set; }
 
         public int TotalLines { get; set; }
 
-        [ContextProperty("Data")]
         public MapImpl Data { get; } = new MapImpl();
+
+        public void AddRange(List<PerformanceInfoMain> performanceInfos)
+        {
+            performanceInfos.ForEach(x => ProcessPerformanceInfo(x));
+        }
+
+        private void ProcessPerformanceInfo(PerformanceInfoMain info)
+        {
+            TotalDurability += info.TotalDurability;
+            info.ModuleData.ForEach(x => ProcessPerformanceInfoModule(x));
+        }
+
+        private void ProcessPerformanceInfoModule(PerformanceInfoModule module)
+        {
+            var moduleBSL = new CoverageModuleId(module.ModuleID);
+            var linesCoverage = Data.Retrieve(moduleBSL) as MapImpl;
+            if (linesCoverage is null)
+            {
+                linesCoverage = new MapImpl();
+                Data.Insert(moduleBSL, linesCoverage);
+            }
+
+            module.LineInfo.ForEach(x => ProcessPerformanceInfoLine(x, linesCoverage));
+        }
+
+        private void ProcessPerformanceInfoLine(PerformanceInfoLine line, MapImpl lineslinesCoverage) =>
+            lineslinesCoverage.Insert(NumberValue.Create(line.LineNo), BooleanValue.True);
+
+        public void Write(string filePath)
+        {
+            var jsonWriter = new JSONWriter();
+            jsonWriter.OpenFile(filePath);
+
+            var jsonFunctions = JsonFunctions.GlobalInstanse();
+
+            jsonWriter.WriteStartArray();
+            foreach (KeyAndValueImpl keyValue in Data)
+            {
+                var moduleId = keyValue.Key.AsObject() as CoverageModuleId;
+                var moduleData = keyValue.Value.AsObject() as MapImpl;
+
+                jsonFunctions.WriteJSON(jsonWriter, Data);
+            }
+
+            jsonWriter.WriteEndArray();
+            jsonWriter.Close();
+        }
     }
 }
