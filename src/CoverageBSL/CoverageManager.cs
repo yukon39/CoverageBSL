29238de43844ea11e738f448a924551edbaede2c
Yuri Goncharuk <yukon39@gmail.com>
2021-05-26 20:42:07 +0300

    feat: Add test connection method to coverage manager

--- src/CoverageBSL/CoverageManager.cs
+++ src/CoverageBSL/CoverageManager.cs
@@ -2,7 +2,6 @@
 using com.github.yukon39.CoverageBSL.Utils;
 using com.github.yukon39.DebugBSL.Client;
 using ScriptEngine;
-using ScriptEngine.Machine;
 using ScriptEngine.Machine.Contexts;
 using System;
 using System.Threading.Tasks;
@@ -21,6 +20,7 @@ namespace com.github.yukon39.CoverageBSL
         private CoverageManager(string debuggerURI) =>
             Client = DebuggerClientFactory.NewInstance(debuggerURI);
 
+
         [ContextMethod("APIVersion", "ВерсияAPI")]
         public string APIVersion()
         {
@@ -38,11 +38,31 @@ namespace com.github.yukon39.CoverageBSL
             }
         }
 
-        [ContextMethod("NewCoverageSession", "НоваяСессияОтладки")]
+        private async Task<string> ApiVersionConfigureAwait() =>
+           await Client.ApiVersionAsync().ConfigureAwait(false);
+
+        [ContextMethod("TestConnection", "ПроверитьСоединение")]
+        public void TestConnection()
+        {
+            try
+            {
+                TestConfigureAwait().GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                var message = Locale.NStr(
+                    "en = 'Test connection error';" +
+                    "ru = 'Ошибка проверки соединения'");
+                Logger.Error(message, e);
+                throw RuntimeExceptionFactory.NewException(message, e);
+            }
+        }
+
+        private async Task TestConfigureAwait() =>
+           await Client.TestAsync().ConfigureAwait(false);
+
+        [ContextMethod("NewCoverageSession", "НоваяСессияПокрытия")]
         public CoverageSession NewCoverageSession(string infobaseAlias) =>
             new CoverageSession(Client, infobaseAlias);
-
-        private async Task<string> ApiVersionConfigureAwait() =>
-            await Client.ApiVersionAsync().ConfigureAwait(false);
     }
 }
