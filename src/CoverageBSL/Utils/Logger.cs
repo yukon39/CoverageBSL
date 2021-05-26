9976504ba9d3f354f03df65e4b34034b54a92025
Yuri Goncharuk <yukon39@gmail.com>
2021-05-26 20:06:37 +0300

    feat: Add Onescript helpers

---
+++ src/CoverageBSL/Utils/Logger.cs
@@ -0,0 +1,29 @@
+﻿using ScriptEngine;
+using ScriptEngine.Machine;
+using System;
+
+namespace com.github.yukon39.CoverageBSL.Utils
+{
+    public class Logger
+    {
+        public static void Error(string message)
+        {
+            SystemLogger.Write(string.Format("[ERROR] {0}", message));
+        }
+
+        public static void Error(string message, Exception e)
+        {
+            string exceptionMessage;
+            if (e is RuntimeException re)
+            {
+                exceptionMessage = re.Message;
+            } 
+            else
+            {
+                exceptionMessage = e.ToString();
+            }
+
+            SystemLogger.Write(string.Format("[ERROR] {0}: {1}", message, exceptionMessage));
+        }
+    }
+}
---
+++ src/CoverageBSL/Utils/RuntimeExceptionFactory.cs
@@ -0,0 +1,28 @@
+﻿using ScriptEngine.Machine;
+using System;
+
+namespace com.github.yukon39.CoverageBSL.Utils
+{
+    public class RuntimeExceptionFactory
+    {
+        public static RuntimeException NewException(string message, Exception e)
+        {
+            var exceptionMessage = string.Format("{0}: {1}", message, ExceptionMessage(e));
+            return new RuntimeException(exceptionMessage, e);
+        }
+
+        private static string ExceptionMessage(Exception e)
+        {
+            if (e.InnerException is Exception inner)
+            {
+                return string.Format("{0} ---> {1}", e.Message, ExceptionMessage(inner));
+
+            }
+            else
+            {
+                return e.Message;
+            }
+        }
+
+    }
+}
