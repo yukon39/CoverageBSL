f5014dca6468806038700b60d60562f21452c4a5
Yuri Goncharuk <yukon39@gmail.com>
2021-05-26 18:58:35 +0300

    feat: Add abstract class to provide session entities managers

---
+++ src/DebugClientBSL/Internal/DebuggerClientEntityManager.cs
@@ -0,0 +1,20 @@
+﻿using com.github.yukon39.DebugBSL.Client.Data;
+using com.github.yukon39.DebugBSL.Client.Internal;
+using System;
+
+namespace com.github.yukon39.DebugBSL.Client.Impl
+{
+    public abstract class DebuggerClientEntityManager
+    {
+        protected readonly DebuggerClientExecutor Executor;
+        protected readonly SessionContext Context;
+
+        public DebuggerClientEntityManager(DebuggerClientExecutor executor, SessionContext context)
+        {
+            Executor = executor;
+            Context = context;
+        }
+        public virtual void SubscribeSessionEvents(IDebuggerClientSession session) =>
+            throw new NotImplementedException();
+    }
+}
