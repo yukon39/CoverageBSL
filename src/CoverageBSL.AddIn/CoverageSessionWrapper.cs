9368876fc5f0e18db2894c9e6cca9e54860ad170
Yuri Goncharuk <yukon39@gmail.com>
2021-05-27 01:07:30 +0300

    refactor: Create coverage session wrapper

---
+++ src/CoverageBSL.AddIn/CoverageSessionWrapper.cs
@@ -0,0 +1,118 @@
+﻿using com.github.yukon39.CoverageBSL.AddIn.Utils;
+using com.github.yukon39.CoverageBSL.Coverage;
+using com.github.yukon39.DebugBSL.debugger.debugBaseData;
+using ScriptEngine;
+using ScriptEngine.HostedScript.Library;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using System;
+
+namespace com.github.yukon39.CoverageBSL.AddIn
+{
+    [ContextClass(typeName: "CoverageSession", typeAlias: "СессияПокрытия")]
+    public class CoverageSessionWrapper : AutoContext<CoverageSessionWrapper>, IObjectWrapper
+    {
+        private readonly CoverageSession session;
+
+        public CoverageSessionWrapper(CoverageSession session) =>
+            this.session = session;
+
+        [ContextMethod("Attach", "Подключить")]
+        public void Attach(string password)
+        {
+            AttachDebugUIResult result;
+            try
+            {
+                result = session.Attach(password);
+            }
+            catch (Exception ex)
+            {
+                var message = Locale.NStr(
+                    "en = 'Attach error';" +
+                    "ru = 'Ошибка подключения'");
+                Logger.Error(message, ex);
+                throw RuntimeExceptionFactory.NewException(message, ex);
+            }
+
+            switch (result)
+            {
+                case AttachDebugUIResult.Registered:
+                    return;
+
+                case AttachDebugUIResult.CredentialsRequired:
+
+                    throw new RuntimeException(
+                        Locale.NStr("en = 'Credentials required';ru = 'Требуется указание пароля'"));
+
+                case AttachDebugUIResult.NotRegistered:
+
+                    throw new RuntimeException(
+                        Locale.NStr("en = 'Not registered';ru = 'Не зарегистрирован'"));
+
+                case AttachDebugUIResult.IBInDebug:
+
+                    throw new RuntimeException(
+                        Locale.NStr("en = 'IB already in debug mode';ru = 'База уже в режиме отладки'"));
+
+                default:
+                    throw new RuntimeException(
+                        Locale.NStr("en = 'Unknown error';ru = 'Неизвестная ошибка'"));
+            }
+        }
+
+        [ContextMethod("Detach", "Отключить")]
+        public void Detach()
+        {
+            try
+            {
+                session.Detach();
+            }
+            catch (Exception ex)
+            {
+                var message = Locale.NStr(
+                    "en = 'Detach error';" +
+                    "ru = 'Ошибка отключения'");
+                Logger.Error(message, ex);
+                throw RuntimeExceptionFactory.NewException(message, ex);
+            }
+        }
+
+        [ContextMethod("StartCoverageCapture", "НачатьСборПокрытия")]
+        public GuidWrapper StartCoverageCapture()
+        {
+            try
+            {
+                var measureId = session.StartCoverageCapture();
+                return new GuidWrapper(measureId.ToString());
+            }
+            catch (Exception ex)
+            {
+                var message = Locale.NStr(
+                    "en = 'StartCoverageCapture error';" +
+                    "ru = 'Ошибка начала сбора покрытия'");
+                Logger.Error(message, ex);
+                throw RuntimeExceptionFactory.NewException(message, ex);
+            }
+        }
+
+        [ContextMethod("StopCoverageCapture", "ЗавершитьСборПокрытия")]
+        public CoverageDataWrapper StopCoverageCapture()
+        {
+            try
+            {
+                var coverageData = session.StopCoverageCapture();
+                return new CoverageDataWrapper(coverageData);
+            }
+            catch (Exception ex)
+            {
+                var message = Locale.NStr(
+                    "en = 'StopCoverageCapture error';" +
+                    "ru = 'Ошибка завершения сбора покрытия'");
+                Logger.Error(message, ex);
+                throw RuntimeExceptionFactory.NewException(message, ex);
+            }
+        }
+
+        public object UnderlyingObject => session;
+    }
+}
--- src/CoverageBSL/Coverage/CoverageSession.cs
+++ src/CoverageBSL/Coverage/CoverageSession.cs
@@ -1,200 +1,83 @@
-﻿using com.github.yukon39.CoverageBSL.Utils;
-using com.github.yukon39.DebugBSL.Client;
+﻿using com.github.yukon39.DebugBSL.Client;
 using com.github.yukon39.DebugBSL.debugger.debugAutoAttach;
 using com.github.yukon39.DebugBSL.debugger.debugBaseData;
-using com.github.yukon39.DebugBSL.debugger.debugMeasure;
 using com.github.yukon39.DebugBSL.debugger.debugRDBGRequestResponse;
-using ScriptEngine;
-using ScriptEngine.HostedScript.Library;
-using ScriptEngine.Machine;
-using ScriptEngine.Machine.Contexts;
-using ScriptEngine.Machine.Values;
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace com.github.yukon39.CoverageBSL.Coverage
 {
-    [ContextClass(typeName: "CoverageSession", typeAlias: "СессияПокрытия")]
-    public class CoverageSession : AutoContext<CoverageSession>
+
+    public class CoverageSession
     {
         private readonly IDebuggerClientSession DebuggerSession;
         private readonly List<DebugTargetType> TargetTypes = DefaultTargetTypes;
         private readonly List<string> AreaNames = new List<string>();
 
-        public CoverageSession(IDebuggerClient debuggerClient, string infobaseAlias) => 
+        public CoverageSession(IDebuggerClient debuggerClient, string infobaseAlias) =>
             DebuggerSession = debuggerClient.CreateSession(infobaseAlias);
 
-        [ContextMethod("Attach", "Подключить")]
-        public void Attach(string password)
+        public AttachDebugUIResult Attach(string password)
         {
-            try
-            {
-                AttachConfigureAwait(password).GetAwaiter().GetResult();
-            }
-            catch (RuntimeException rex)
-            {
-                Logger.Error(rex.ErrorDescription, rex);
-                throw;
-            }
-            catch (Exception ex)
-            {
-                var message = Locale.NStr("en = 'Attach error';ru = 'Ошибка подключения'");
-                Logger.Error(message, ex);
-                throw RuntimeExceptionFactory.NewException(message, ex);
-            }
+            var safePassword = password.ToCharArray();
+            var options = new DebuggerOptions();
+            return AttachConfigureAwait(safePassword, options).GetAwaiter().GetResult();
         }
 
-        private async Task AttachConfigureAwait(string password) =>
-            await AttachAsync(password).ConfigureAwait(false);
+        private async Task<AttachDebugUIResult> AttachConfigureAwait(char[] password, DebuggerOptions options) =>
+            await AttachAsync(password, options).ConfigureAwait(false);
 
-        private async Task AttachAsync(string password)
+        private async Task<AttachDebugUIResult> AttachAsync(char[] password, DebuggerOptions options)
         {
-            var attachResult = await DebuggerSession.AttachAsync(password.ToCharArray(), new DebuggerOptions());
+            var result = await DebuggerSession.AttachAsync(password, options).ConfigureAwait(false);
 
-            switch (attachResult)
+            if (DebuggerSession.IsAttached())
             {
-                case AttachDebugUIResult.Registered:
-                    await OnSuccessfulAttachAsync();
-                    break;
+                var data = new HTTPServerInitialDebugSettingsData();
 
-                case AttachDebugUIResult.CredentialsRequired:
+                var autoAttachSettings = new DebugAutoAttachSettings();
+                autoAttachSettings.TargetType.AddRange(TargetTypes);
+                autoAttachSettings.AreaName.AddRange(AreaNames);
 
-                    throw new RuntimeException(
-                        Locale.NStr("en = 'Credentials required';ru = 'Требуется указание пароля'"));
+                var targetsManager = DebuggerSession.GetTargetsManager();
 
-                case AttachDebugUIResult.NotRegistered:
-
-                    throw new RuntimeException(
-                        Locale.NStr("en = 'Not registered';ru = 'Не зарегистрирован'"));
-
-                case AttachDebugUIResult.IBInDebug:
-
-                    throw new RuntimeException(
-                        Locale.NStr("en = 'IB already in debug mode';ru = 'База уже в режиме отладки'"));
-
-                default:
-                    throw new RuntimeException(
-                        Locale.NStr("en = 'Unknown error';ru = 'Неизвестная ошибка'"));
+                await targetsManager.InitSettingsAsync(data);
+                await targetsManager.SetAutoAttachSettingsAsync(autoAttachSettings);
+                (await targetsManager.AttachedTargetsStatesAsync(""))
+                    .ForEach(async x => await targetsManager.AttachDebugTargetAsync(x.TargetID.TargetIdLight));
             }
 
+            return result;
         }
 
-        private async Task OnSuccessfulAttachAsync()
-        {
-            var data = new HTTPServerInitialDebugSettingsData();
-
-            var autoAttachSettings = new DebugAutoAttachSettings();
-            autoAttachSettings.TargetType.AddRange(TargetTypes);
-            autoAttachSettings.AreaName.AddRange(AreaNames);
-
-            var targetsManager = DebuggerSession.GetTargetsManager();
-
-            await targetsManager.InitSettingsAsync(data);
-            await targetsManager.SetAutoAttachSettingsAsync(autoAttachSettings);
-            (await targetsManager.AttachedTargetsStatesAsync(""))
-                .ForEach(async x => await targetsManager.AttachDebugTargetAsync(x.TargetID.TargetIdLight));
-        }
-
-        [ContextMethod("Detach", "Отключить")]
-        public void Detach()
-        {
-            try
-            {
-                DetachConfigureAwait().GetAwaiter().GetResult();
-            }
-            catch (Exception ex)
-            {
-                var message = Locale.NStr("en = 'Detach error';ru = 'Ошибка отключения'");
-                Logger.Error(message, ex);
-                throw RuntimeExceptionFactory.NewException(message, ex);
-            }
-        }
+        public void Detach() =>
+             DetachConfigureAwait().GetAwaiter().GetResult();
 
         private async Task DetachConfigureAwait() =>
             await DebuggerSession.DetachAsync().ConfigureAwait(false);
 
-        [ContextMethod("StartCoverageCapture", "НачатьСборПокрытия")]
-        public GuidWrapper StartCoverageCapture()
-        {
-            try
-            {
-                return StartPerformanceMeasureConfigureAwait().GetAwaiter().GetResult();
-            }
-            catch (Exception ex)
-            {
-                var message = Locale.NStr(
-                    "en = 'StartCoverageCapture error';" +
-                    "ru = 'Ошибка начала сбора покрытия'");
-                Logger.Error(message, ex);
-                throw RuntimeExceptionFactory.NewException(message, ex);
-            }
-        }
+        public Guid StartCoverageCapture() =>
+            StartPerformanceMeasureConfigureAwait().GetAwaiter().GetResult();
 
-        private async Task<GuidWrapper> StartPerformanceMeasureConfigureAwait() =>
-            await StartPerformanceMeasureAsync().ConfigureAwait(false);
+        private async Task<Guid> StartPerformanceMeasureConfigureAwait() =>
+            await DebuggerSession.GetMeasureManager().StartMeasureModeAsync().ConfigureAwait(false);
 
-        private async Task<GuidWrapper> StartPerformanceMeasureAsync()
-        {
-            var measureManager = DebuggerSession.GetMeasureManager();
-            var measureId = await measureManager.StartMeasureModeAsync();
-            return new GuidWrapper(measureId.ToString());
-        }
-
-        [ContextMethod("StopCoverageCapture", "ЗавершитьСборПокрытия")]
         public CoverageData StopCoverageCapture()
-        {
-            try
-            {
-                var performanceInfo = StopPerformanceMeasureConfigureAwait().GetAwaiter().GetResult();
+            => StopCoverageCaptureConfigureAwait().GetAwaiter().GetResult();
 
-                var coverageData = new CoverageData();
-                performanceInfo.ForEach(x => ProcessPerformanceInfo(coverageData, x));
+        private async Task<CoverageData> StopCoverageCaptureConfigureAwait() =>
+            await StopCoverageCaptureAsync().ConfigureAwait(false);
 
-                return coverageData;
-
-            }
-            catch (Exception ex)
-            {
-                var message = Locale.NStr(
-                    "en = 'StopCoverageCapture error';" +
-                    "ru = 'Ошибка завершения сбора покрытия'");
-                Logger.Error(message, ex);
-                throw RuntimeExceptionFactory.NewException(message, ex);
-            }
-        }
-
-        private async Task<List<PerformanceInfoMain>> StopPerformanceMeasureConfigureAwait() =>
-            await StopPerformanceMeasureAsync().ConfigureAwait(false);
-
-        private async Task<List<PerformanceInfoMain>> StopPerformanceMeasureAsync()
+        private async Task<CoverageData> StopCoverageCaptureAsync()
         {
             var measureManager = DebuggerSession.GetMeasureManager();
-            return await measureManager.StopMeasureModeAsync();
-        }
-        
-        private void ProcessPerformanceInfo(CoverageData coverageData, PerformanceInfoMain info)
-        {
-            coverageData.TotalDurability += info.TotalDurability;
-            info.ModuleData.ForEach(x => ProcessPerformanceInfoModule(coverageData, x));
-        }
+            var performanceInfo = await measureManager.StopMeasureModeAsync();
 
-        private void ProcessPerformanceInfoModule(CoverageData coverageData, PerformanceInfoModule module)
-        {
-            var moduleBSL = new CoverageModuleId(module.ModuleID);
-            var linesCoverage = coverageData.Data.Retrieve(moduleBSL) as MapImpl;
-            if (linesCoverage == null)
-            {
-                linesCoverage = new MapImpl();
-                coverageData.Data.Insert(moduleBSL, linesCoverage);
-            }
-
-            module.LineInfo.ForEach(x => ProcessPerformanceInfoLine(x, linesCoverage));
+            var coverageData = new CoverageData();
+            coverageData.AddRange(performanceInfo);
+            return coverageData;
         }
-
-        private void ProcessPerformanceInfoLine(PerformanceInfoLine line, MapImpl lineslinesCoverage) =>
-            lineslinesCoverage.Insert(NumberValue.Create(line.LineNo), BooleanValue.True);
 
         private static List<DebugTargetType> DefaultTargetTypes => new List<DebugTargetType>()
         {
