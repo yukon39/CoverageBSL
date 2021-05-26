37742f587a3b9adec5998d1385432af1588112d2
Yuri Goncharuk <yukon39@gmail.com>
2021-05-27 01:06:59 +0300

    refactor: Create coverage manager wrapper

---
+++ src/CoverageBSL.AddIn/CoverageManagerWrapper.cs
@@ -0,0 +1,63 @@
+﻿using com.github.yukon39.CoverageBSL.AddIn.Utils;
+using ScriptEngine;
+using ScriptEngine.Machine.Contexts;
+using System;
+
+namespace com.github.yukon39.CoverageBSL.AddIn
+{
+    [ContextClass(typeName: "CoverageManager", typeAlias: "МенеджерПокрытия")]
+    public class CoverageManagerWrapper : AutoContext<CoverageManagerWrapper>, IObjectWrapper
+    {
+        private readonly CoverageManager manager;
+
+        [ScriptConstructor]
+        public static CoverageManagerWrapper ScriptConstructor(string debuggerURI) =>
+            new CoverageManagerWrapper(debuggerURI);
+
+        private CoverageManagerWrapper(string debuggerURI) =>
+            manager = new CoverageManager(debuggerURI);
+
+        [ContextMethod("APIVersion", "ВерсияAPI")]
+        public string APIVersion()
+        {
+            try
+            {
+                return manager.APIVersion();
+            }
+            catch (Exception e)
+            {
+                var message = Locale.NStr(
+                   "en = 'Get API version error';" +
+                   "ru = 'Ошибка получения версии API'");
+                Logger.Error(message, e);
+                throw RuntimeExceptionFactory.NewException(message, e);
+            }
+        }
+
+        [ContextMethod("TestConnection", "ПроверитьСоединение")]
+        public void TestConnection()
+        {
+            try
+            {
+                manager.TestConnection();
+            }
+            catch (Exception e)
+            {
+                var message = Locale.NStr(
+                    "en = 'Test connection error';" +
+                    "ru = 'Ошибка проверки соединения'");
+                Logger.Error(message, e);
+                throw RuntimeExceptionFactory.NewException(message, e);
+            }
+        }
+
+        [ContextMethod("NewCoverageSession", "НоваяСессияПокрытия")]
+        public CoverageSessionWrapper NewCoverageSession(string infobaseAlias)
+        {
+            var session = manager.NewCoverageSession(infobaseAlias);
+            return new CoverageSessionWrapper(session);
+        }
+
+        public object UnderlyingObject => manager;
+    }
+}
--- src/CoverageBSL/CoverageManager.cs
+++ src/CoverageBSL/CoverageManager.cs
@@ -1,67 +1,28 @@
 ﻿using com.github.yukon39.CoverageBSL.Coverage;
-using com.github.yukon39.CoverageBSL.Utils;
 using com.github.yukon39.DebugBSL.Client;
-using ScriptEngine;
-using ScriptEngine.Machine.Contexts;
-using System;
 using System.Threading.Tasks;
 
 namespace com.github.yukon39.CoverageBSL
 {
-    [ContextClass(typeName: "CoverageManager", typeAlias: "МенеджерПокрытия")]
-    public class CoverageManager : AutoContext<CoverageManager>
+    public class CoverageManager
     {
         private readonly IDebuggerClient Client;
 
-        [ScriptConstructor]
-        public static CoverageManager Constructor(string debuggerURI) =>
-            new CoverageManager(debuggerURI);
-
-        private CoverageManager(string debuggerURI) =>
+        public CoverageManager(string debuggerURI) =>
             Client = DebuggerClientFactory.NewInstance(debuggerURI);
 
-
-        [ContextMethod("APIVersion", "ВерсияAPI")]
-        public string APIVersion()
-        {
-            try
-            {
-                return ApiVersionConfigureAwait().GetAwaiter().GetResult();
-            }
-            catch (Exception e)
-            {
-                var message = Locale.NStr(
-                    "en = 'Get API version error';" +
-                    "ru = 'Ошибка получения версии API'");
-                Logger.Error(message, e);
-                throw RuntimeExceptionFactory.NewException(message, e);
-            }
-        }
+        public string APIVersion() =>
+            ApiVersionConfigureAwait().GetAwaiter().GetResult();
 
         private async Task<string> ApiVersionConfigureAwait() =>
            await Client.ApiVersionAsync().ConfigureAwait(false);
 
-        [ContextMethod("TestConnection", "ПроверитьСоединение")]
-        public void TestConnection()
-        {
-            try
-            {
-                TestConfigureAwait().GetAwaiter().GetResult();
-            }
-            catch (Exception e)
-            {
-                var message = Locale.NStr(
-                    "en = 'Test connection error';" +
-                    "ru = 'Ошибка проверки соединения'");
-                Logger.Error(message, e);
-                throw RuntimeExceptionFactory.NewException(message, e);
-            }
-        }
+        public void TestConnection() =>
+            TestConfigureAwait().GetAwaiter().GetResult();
 
         private async Task TestConfigureAwait() =>
            await Client.TestAsync().ConfigureAwait(false);
 
-        [ContextMethod("NewCoverageSession", "НоваяСессияПокрытия")]
         public CoverageSession NewCoverageSession(string infobaseAlias) =>
             new CoverageSession(Client, infobaseAlias);
     }
