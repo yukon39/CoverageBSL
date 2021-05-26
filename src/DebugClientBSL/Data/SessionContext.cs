a2549550715315cfd9d9770ff774752f0cfc8f7a
Yuri Goncharuk <yukon39@gmail.com>
2021-05-26 18:46:27 +0300

    feat: Add debugger session context holder class

---
+++ src/DebugClientBSL/Data/SessionContext.cs
@@ -0,0 +1,32 @@
+﻿using com.github.yukon39.DebugBSL.debugger.debugRDBGRequestResponse;
+using System;
+
+namespace com.github.yukon39.DebugBSL.Client.Data
+{
+    public class SessionContext
+    {
+        private readonly string InfobaseAlias;
+        public readonly Guid DebugSession;
+
+        private SessionContext(string infobaseAlias, Guid debugSession)
+        {
+            InfobaseAlias = infobaseAlias;
+            DebugSession = debugSession;
+        }
+
+        public static SessionContext NewInstance(string infobaseAlias, Guid debugSession) =>
+            new SessionContext(infobaseAlias, debugSession);
+
+        public static SessionContext NewInstance(string infobaseAlias) =>
+            new SessionContext(infobaseAlias, Guid.NewGuid());
+
+        public T NewSessionRequest<T>() where T : RDbgBaseRequest
+        {
+            var request = Activator.CreateInstance<T>();
+            request.IdOfDebuggerUI = DebugSession;
+            request.InfoBaseAlias = InfobaseAlias;
+
+            return request;
+        }
+    }
+}
