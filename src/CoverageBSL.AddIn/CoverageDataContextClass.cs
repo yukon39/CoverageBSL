1a6d1e83803e0828ad33b19ae431d2aaafa519a2
Yuri Goncharuk <yukon39@gmail.com>
2021-05-27 21:03:24 +0300

    feat: Coverage data now more json-friendly

--- src/CoverageBSL.AddIn/CoverageDataContextClass.cs
+++ src/CoverageBSL.AddIn/CoverageDataContextClass.cs
@@ -1,5 +1,9 @@
-﻿using ScriptEngine.HostedScript.Library;
+﻿using com.github.yukon39.DebugBSL.debugger.debugBaseData;
+using com.github.yukon39.DebugBSL.debugger.debugMeasure;
+using ScriptEngine.HostedScript.Library;
 using ScriptEngine.Machine.Contexts;
+using ScriptEngine.Machine.Values;
+using System.Collections.Generic;
 
 namespace com.github.yukon39.CoverageBSL.AddIn
 {
@@ -10,12 +14,56 @@ namespace com.github.yukon39.CoverageBSL.AddIn
         public long TotalDurability { get; }
 
         [ContextProperty("Data")]
-        public MapImpl Data { get; }
+        public ArrayImpl Data { get; } = new ArrayImpl();
 
         public CoverageDataContextClass(ICoverageData coverageData)
         {
             TotalDurability = coverageData.TotalDurability;
-            Data = coverageData.Data;
+            AddRange(coverageData.Data);
+        }
+
+        private void AddRange(List<PerformanceInfoMain> performanceInfos)
+        {
+            performanceInfos.ForEach(x => ProcessPerformanceInfo(x));
+        }
+
+        private void ProcessPerformanceInfo(PerformanceInfoMain info)
+        {
+            info.ModuleData.ForEach(x => ProcessPerformanceInfoModule(x));
+        }
+
+        private void ProcessPerformanceInfoModule(PerformanceInfoModule module)
+        {
+            var moduleBSL = ModuleId(module.ModuleID);
+            var linesCoverage = new ArrayImpl();
+
+            module.LineInfo.ForEach(x => ProcessPerformanceInfoLine(x, linesCoverage));
+
+            moduleBSL.Insert("LineNo", linesCoverage);
+            Data.Add(moduleBSL);
+        }
+
+        private static void ProcessPerformanceInfoLine(PerformanceInfoLine line, ArrayImpl linesCoverage)
+        {
+            var result = new StructureImpl();
+            result.Insert("lineNumber", NumberValue.Create(line.LineNo));
+            result.Insert("covered", BooleanValue.True);
+
+            linesCoverage.Add(result);
+        }
+
+        private static StructureImpl ModuleId(BSLModuleIdInternal moduleId)
+        {
+            var result = new StructureImpl();
+            result.Insert("ModuleType", StringValue.Create(moduleId.Type.ToString()));
+            result.Insert("URL", StringValue.Create(moduleId.URL));
+            result.Insert("ExtensionName", StringValue.Create(moduleId.ExtensionName));
+            result.Insert("ObjectID", StringValue.Create(moduleId.ObjectID.ToString()));
+            result.Insert("PropertyID", StringValue.Create(moduleId.PropertyID.ToString()));
+            result.Insert("ExtId", NumberValue.Create(moduleId.ExtId));
+            result.Insert("Version", StringValue.Create(moduleId.Version));
+
+            return result;
         }
     }
 }
--- src/CoverageBSL/Impl/CoverageData.cs
+++ src/CoverageBSL/Impl/CoverageData.cs
@@ -1,8 +1,4 @@
 ﻿using com.github.yukon39.DebugBSL.debugger.debugMeasure;
-using ScriptEngine.HostedScript.Library;
-using ScriptEngine.HostedScript.Library.Json;
-using ScriptEngine.Machine;
-using ScriptEngine.Machine.Values;
 using System.Collections.Generic;
 
 namespace com.github.yukon39.CoverageBSL.Coverage.Impl
@@ -13,33 +9,15 @@ namespace com.github.yukon39.CoverageBSL.Coverage.Impl
 
         public int TotalLines { get; set; }
 
-        public MapImpl Data { get; } = new MapImpl();
+        public List<PerformanceInfoMain> Data { get; } = new List<PerformanceInfoMain>();
 
-        public void AddRange(List<PerformanceInfoMain> performanceInfos)
-        {
+        public void AddRange(List<PerformanceInfoMain> performanceInfos) =>
             performanceInfos.ForEach(x => ProcessPerformanceInfo(x));
-        }
 
         private void ProcessPerformanceInfo(PerformanceInfoMain info)
         {
             TotalDurability += info.TotalDurability;
-            info.ModuleData.ForEach(x => ProcessPerformanceInfoModule(x));
+            Data.Add(info);
         }
-
-        private void ProcessPerformanceInfoModule(PerformanceInfoModule module)
-        {
-            var moduleBSL = new CoverageModuleId(module.ModuleID);
-            var linesCoverage = Data.Retrieve(moduleBSL) as MapImpl;
-            if (linesCoverage is null)
-            {
-                linesCoverage = new MapImpl();
-                Data.Insert(moduleBSL, linesCoverage);
-            }
-
-            module.LineInfo.ForEach(x => ProcessPerformanceInfoLine(x, linesCoverage));
-        }
-
-        private void ProcessPerformanceInfoLine(PerformanceInfoLine line, MapImpl lineslinesCoverage) =>
-            lineslinesCoverage.Insert(NumberValue.Create(line.LineNo), BooleanValue.True);
     }
 }
