a78469869c4fd20a30b70888a8bea228334181a0
Yuri Goncharuk <yukon39@gmail.com>
2021-06-03 22:07:41 +0300

    refactor: Use context class for module coverage info

--- src/CoverageBSL.AddIn/CoverageDataContextClass.cs
+++ src/CoverageBSL.AddIn/CoverageDataContextClass.cs
@@ -23,23 +23,13 @@ namespace com.github.yukon39.CoverageBSL.AddIn
             AddRange(coverageData.Data);
         }
 
-        private void AddRange(List<PerformanceInfoMain> performanceInfos)
-        {
-            performanceInfos.ForEach(x => ProcessPerformanceInfo(x));
-        }
+        private void AddRange(List<PerformanceInfoMain> performanceInfos) 
+            => performanceInfos.ForEach(x => ProcessPerformanceInfo(x));
 
-        private void ProcessPerformanceInfo(PerformanceInfoMain info)
-        {
-            info.ModuleData.ForEach(x => ProcessPerformanceInfoModule(x));
-        }
+        private void ProcessPerformanceInfo(PerformanceInfoMain info) 
+            => info.ModuleData.ForEach(x => ProcessPerformanceInfoModule(x));
 
-        private void ProcessPerformanceInfoModule(PerformanceInfoModule module)
-        {
-            var moduleBSL = new CoverageModuleIdWrapper(module.ModuleID);
-            var linesCoverage = new CoverageLineInfosContextClass(module.LineInfo);
-
-            //moduleBSL.Insert("LineNo", linesCoverage);
-            Data.Add(moduleBSL);
-        }
+        private void ProcessPerformanceInfoModule(PerformanceInfoModule module) 
+            => Data.Add(new CoverageModuleInfoContextClass(module));
     }
 }
---
+++ src/CoverageBSL.AddIn/Debugger/CoverageModuleInfoContextClass.cs
@@ -0,0 +1,18 @@
+﻿using com.github.yukon39.DebugBSL.debugger.debugMeasure;
+using ScriptEngine.Machine.Contexts;
+
+namespace com.github.yukon39.CoverageBSL.AddIn.Debugger
+{
+    [ContextClass(typeName: "CoverageModuleInfo", typeAlias: "ПокрытиеСтрокМодуля")]
+    class CoverageModuleInfoContextClass : AutoContext<CoverageModuleInfoContextClass>
+    {
+        private readonly CoverageModuleIdWrapper moduleId;
+        private readonly CoverageLineInfosContextClass lineInfos;
+
+        public CoverageModuleInfoContextClass(PerformanceInfoModule moduleInfo)
+        {
+            moduleId = new CoverageModuleIdWrapper(moduleInfo.ModuleID);
+            lineInfos = new CoverageLineInfosContextClass(moduleInfo.LineInfo);
+        }
+    }
+}
