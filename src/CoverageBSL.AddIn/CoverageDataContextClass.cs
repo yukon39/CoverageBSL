4c738f36bdcac3f4400b99ea3106f39ac57ed41d
Yuri Goncharuk <yukon39@gmail.com>
2021-06-08 23:18:59 +0300

    refactor: Use context class to manage list

--- src/CoverageBSL.AddIn/CoverageDataContextClass.cs
+++ src/CoverageBSL.AddIn/CoverageDataContextClass.cs
@@ -1,10 +1,7 @@
 ﻿using com.github.yukon39.CoverageBSL.AddIn.Debugger;
-using com.github.yukon39.DebugBSL.debugger.debugBaseData;
-using com.github.yukon39.DebugBSL.debugger.debugMeasure;
-using ScriptEngine.HostedScript.Library;
+using ScriptEngine.HostedScript.Library.Json;
 using ScriptEngine.Machine.Contexts;
 using ScriptEngine.Machine.Values;
-using System.Collections.Generic;
 
 namespace com.github.yukon39.CoverageBSL.AddIn
 {
@@ -15,21 +12,11 @@ namespace com.github.yukon39.CoverageBSL.AddIn
         public long TotalDurability { get; }
 
         [ContextProperty("Data")]
-        public ArrayImpl Data { get; } = new ArrayImpl();
-
+        public CoverageModuleInfoList Data { get; }
         public CoverageDataContextClass(ICoverageData coverageData)
         {
             TotalDurability = coverageData.TotalDurability;
-            AddRange(coverageData.Data);
+            Data = new CoverageModuleInfoList(coverageData.Data);
         }
-
-        private void AddRange(List<PerformanceInfoMain> performanceInfos) 
-            => performanceInfos.ForEach(x => ProcessPerformanceInfo(x));
-
-        private void ProcessPerformanceInfo(PerformanceInfoMain info) 
-            => info.ModuleData.ForEach(x => ProcessPerformanceInfoModule(x));
-
-        private void ProcessPerformanceInfoModule(PerformanceInfoModule module) 
-            => Data.Add(new CoverageModuleInfoContextClass(module));
     }
 }
---
+++ src/CoverageBSL.AddIn/Debugger/CoverageModuleInfoList.cs
@@ -0,0 +1,37 @@
+﻿using com.github.yukon39.DebugBSL.debugger.debugMeasure;
+using ScriptEngine.HostedScript.Library.Json;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace com.github.yukon39.CoverageBSL.AddIn.Debugger
+{
+    [ContextClass(typeName: "CoverageModuleInfoList", typeAlias: "СписокПокрытияМодулей")]
+    public class CoverageModuleInfoList : AutoContext<CoverageModuleInfoList>, ICollectionContext, IEnumerable<IValue>
+    {
+        private readonly List<CoverageModuleInfoContextClass> moduleInfos = new List<CoverageModuleInfoContextClass>();
+
+        public CoverageModuleInfoList(List<PerformanceInfoMain> performanceInfos)
+            => performanceInfos.ForEach(x => ProcessPerformanceInfo(x));
+
+        private void ProcessPerformanceInfo(PerformanceInfoMain info)
+            => info.ModuleData.ForEach(x => ProcessPerformanceInfoModule(x));
+
+        private void ProcessPerformanceInfoModule(PerformanceInfoModule module)
+            => moduleInfos.Add(new CoverageModuleInfoContextClass(module));
+
+        [ContextMethod("Count", "Количество")]
+        public int Count()
+            => moduleInfos.Count;
+
+        public IEnumerator<IValue> GetEnumerator()
+            => moduleInfos.GetEnumerator();
+
+        public CollectionEnumerator GetManagedIterator()
+            => new CollectionEnumerator(GetEnumerator());
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}
