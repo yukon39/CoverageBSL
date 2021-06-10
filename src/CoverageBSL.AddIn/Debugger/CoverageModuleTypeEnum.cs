eb80839f1acb156b3f85d9074f3bb9b9731a23d9
Yuri Goncharuk <yukon39@gmail.com>
2021-06-10 07:47:30 +0300

    feat: Add BSLModuleType wrapper enum

---
+++ src/CoverageBSL.AddIn/Debugger/CoverageModuleTypeEnum.cs
@@ -0,0 +1,24 @@
+﻿using com.github.yukon39.DebugBSL.debugger.debugBaseData;
+using ScriptEngine;
+
+namespace com.github.yukon39.CoverageBSL.AddIn.Debugger
+{
+    [EnumerationType("CoverageModuleType", "ТипМодуляПокрытия")]
+    public enum CoverageModuleTypeEnum
+    {
+        [EnumItem("ConfigModule", "МодульКонфигурации")]
+        ConfigModule = BSLModuleType.ConfigModule,
+
+        [EnumItem("SystemFormModule", "МодульСистемнойФормы")]
+        SystemFormModule = BSLModuleType.SystemFormModule,
+
+        [EnumItem("SystemModule", "МодульСистемы")]
+        SystemModule = BSLModuleType.SystemModule,
+
+        [EnumItem("ExtMDModule", "ВнешнийМодуль")]
+        ExtMDModule = BSLModuleType.ExtMDModule,
+
+        [EnumItem("ExtensionModule", "МодульРасширения")]
+        ExtensionModule = BSLModuleType.ExtensionModule
+    }
+}
